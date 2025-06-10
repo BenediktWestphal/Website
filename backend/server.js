efbcdca7574c40add663cd97346659fc3dd2a464
BenediktWestphal <150813772+BenediktWestphal@users.noreply.github.com>
2025-06-10 23:07:14 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,12 @@ const { pool, createProjectsTable, query } = require('./db');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+const allowedOrigin = process.env.FRONTEND_URL;
+
+app.use(cors({
+  origin: allowedOrigin,
+  credentials: true
+}));
 
 
  // Default CORS configuration allows all origins
